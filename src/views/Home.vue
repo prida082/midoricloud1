cf335a4788821fd41955abf323cea923f466e96a
prida082 <beatbrave882@gmail.com>
2021-03-12 19:45:00 +0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,14 @@
     <div class="page-content">
       <b-container>
         <br />
-        <h1>Welcome to MIDORI</h1>
+        <h1>Welcome To MIDORI</h1>
         <b-img
           src="https://tuemaster.com/wp-content/uploads/2020/05/583651-topic-ix-23-1024x512.jpg"
         ></b-img>
       </b-container>
       <b-container>
         <br />
-        <b-button href="/register" variant="primary">Sign in now</b-button>
+        <b-button href="/register" variant="primary">Sign In Now</b-button>
       </b-container>
     </div>
   </div>
